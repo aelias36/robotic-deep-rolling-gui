cfa3ad249b36462026a8ec86a9764af607dab44c
Alex Elias <aelias36@yahoo.com>
2021-11-01 19:16:58 -0400

    new test for clearing buffer

--- src/rpi_abb_irc5/test_egm.py
+++ src/rpi_abb_irc5/test_egm.py
@@ -7,8 +7,23 @@ try:
 	while True:
 		res, state = egm.receive_from_robot(.1)
 		if res:
+			# Clear queue
+			i = 0
+			while True:
+				res_i, state_i = egm.receive_from_robot()
+				if res_i: # there was another msg waiting
+					state = state_i
+					i += 1
+				else: # previous msg was end of queue
+					break
+
+			if i > 0:
+				print("Number of extra msgs in queue: ", i)
+
 			q_c = np.deg2rad(state.joint_angles)
 			egm.send_to_robot(q_c)
-		print(res, state)
+			#print(q_c)
+		#print(res, state)
+
 except KeyboardInterrupt:
 	raise
